30ff1caaca54dcec8f254ce7292f9b3a35766fde
Melih Ulusoy <amelihulusoy@gmail.com>
2023-10-24 16:24:49 +0300

    self-signed certificate and helmet.js added

---
+++ Node/security-example/server.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const express = require("express");
+const helmet = require("helmet");
+
+const PORT = 3000;
+
+const app = express();
+
+// Helmet Middleware
+app.use(helmet());
+
+app.get("/secret", (req, res) => {
+  return res.send("My secret data is...");
+});
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// HTTPS with SSL Certificate
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
