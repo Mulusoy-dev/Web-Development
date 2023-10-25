ec36fc3ea3b2584aa32a2aa1446699b6ea23034f
Melih Ulusoy <amelihulusoy@gmail.com>
2023-10-25 17:24:14 +0300

    .env file is added

--- Node/security-example/server.js
+++ Node/security-example/server.js
@@ -4,15 +4,40 @@ const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
 
+require("dotenv").config();
+
 const PORT = 3000;
 
 const app = express();
 
+// Credential Information
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
 // Helmet Middleware
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
-  return res.send("My secret data is...");
+// Authorization in Express
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.send("My secret data is Malatya");
 });
 
 app.get("/", (req, res) => {
