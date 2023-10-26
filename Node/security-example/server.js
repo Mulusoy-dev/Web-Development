9e3ccb70206598d5e4e57b87df267e10049e6d49
Melih Ulusoy <amelihulusoy@gmail.com>
2023-10-26 09:34:36 +0300

    google oauth strategy is added

--- Node/security-example/server.js
+++ Node/security-example/server.js
@@ -3,12 +3,12 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
 require("dotenv").config();
 
 const PORT = 3000;
-
-const app = express();
 
 // Credential Information
 const config = {
@@ -16,8 +16,26 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+
 // Helmet Middleware
 app.use(helmet());
+
+// Passport must be under helmet because related to security
+app.use(passport.initialize());
 
 // Authorization in Express
 function checkLoggedIn(req, res, next) {
@@ -30,14 +48,28 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
+app.get("/auth/google", passport.authenticate("google", { scope: ["email"] }));
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("My secret data is Malatya");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
 });
 
 app.get("/", (req, res) => {
