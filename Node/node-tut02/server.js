9fd1807c67cc9041e0309585e7c5821495ad411f
Melih Ulusoy <amelihulusoy@gmail.com>
2023-12-22 17:10:23 +0300

    eventemitter is added

---
+++ Node/node-tut02/server.js
@@ -0,0 +1,10 @@
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+
+// Initiliaze object
+const myEmitter = new Emitter();
+
+// myEmitter.on("log", (msg) => logEvents(msg));
+
+// myEmitter.emit("log", "Log event emitted!");
