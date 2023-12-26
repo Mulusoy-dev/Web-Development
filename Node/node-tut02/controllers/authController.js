e29130589b49293512b494e15fd8c5e7eb731984
Melih Ulusoy <amelihulusoy@gmail.com>
2023-12-26 16:03:08 +0300

    accessToken and refreshToken are added

--- Node/node-tut02/controllers/authController.js
+++ Node/node-tut02/controllers/authController.js
@@ -5,7 +5,13 @@ const usersDB = {
   },
 };
 
+// bcrypt definition
 const bcrypt = require("bcrypt");
+// jwt definiton
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
@@ -21,7 +27,39 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
     // create JWTs
-    res.json({ message: `User ${user} is logged in!` });
+    const accessToken = jwt.sign(
+      { username: foundUser.username }, // Payload -> JWT içinde taşınacak veri
+      process.env.ACCESS_TOKEN_SECRET, // Secret Key -> JWT'nin imzalanması için kullanılan gizli anahtardır.
+      { expiresIn: "30s" } // Options -> JWT geçerliliğini ve diğer ayarları belirten bir dizi seçenek içerir. (Burada, JWT 30s geçerlilik süresi olarak belirtilmiş.)
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username }, // Payload -> JWT içinde taşınacak veri
+      process.env.REFRESH_TOKEN_SECRET, // Secret Key -> JWT'nin imzalanması için kullanılan gizli anahtardır.
+      { expiresIn: "1d" } // Options -> JWT geçerliliğini ve diğer ayarları belirten bir dizi seçenek içerir. (Burada, JWT 1 gün geçerlilik süresi olarak belirtilmiş.)
+    );
+
+    // Aktif kullanıcı için refreshToken kaydetme
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    // İstemciye 'refreshToken' ve 'accessToken' gerekli işlemleri yapması için gönderilir.
+    // accessToken istemciye gönderilen JSON formatı içinde yer alır. refreshToken bir HTTP çerezine (cookie) eklenir.
+    // httpOnly: true özelliği sayesinde tarayıcı üzerinden çerezin okunmasını ve değiştirilmesini önler.
+    // refresh token'ı bir HTTP çerezi içinde göndermenin temel amacı, tarayıcı tabanlı saldırıları (XSS gibi) engellemektir.
+    // refresh token httpOnly özelliği ile sadece sunucu tarafından okunabilen bir çerezde saklandığı için, XSS saldırıları bu çereze erişemez.
+    // Bu çerez JavaScript tarafından okunamaz.
+    res.cookie("jwt", refreshToken, {
+      // 'jwt' -> çerez adı
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    }); // 1 gün geçerli
+    res.json({ accessToken });
   } else {
     res.sendStatus(401); // Unauthorized
   }
