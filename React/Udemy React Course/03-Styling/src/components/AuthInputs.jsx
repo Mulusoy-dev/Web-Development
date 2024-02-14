322b99d8fb2a724357c2a4f9aa5f2f2d11a6b537
Melih Ulusoy <amelihulusoy@gmail.com>
2024-02-14 15:20:42 +0300

    conditionally styled components are added

--- React/Udemy React Course/03-Styling/src/components/AuthInputs.jsx
+++ React/Udemy React Course/03-Styling/src/components/AuthInputs.jsx
@@ -1,13 +1,67 @@
 import { useState } from "react";
-import "./AuthInputs.css";
-
 import { styled } from "styled-components";
+
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
+
+  & label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: #6b7280;
+  }
+
+  & input {
+    width: 100%;
+    padding: 0.75rem 1rem;
+    line-height: 1.5;
+    background-color: #d1d5db;
+    color: #374151;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  }
+`;
+
+const AuthInputsContainer = styled.div`
+  width: 100%;
+  max-width: 28rem;
+  padding: 2rem;
+  margin: 0 auto;
+  border-radius: 0.5rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  background: linear-gradient(180deg, #474232 0%, #28271c 100%);
+  color: white;
+`;
+
+const ActionsContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+`;
+
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+  cursor: pointer;
+  background: none;
+  line-height: inherit;
+
+  &:hover {
+    color: #f0920e;
+  }
+  &:focus {
+    outline: none;
+  }
 `;
 
 export default function AuthInputs() {
@@ -29,42 +83,31 @@ export default function AuthInputs() {
 
   const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+  console.log(emailNotValid, passwordNotValid);
 
   return (
-    <div id="auth-inputs">
+    <AuthInputsContainer>
       <ControlContainer>
-        <p>
-          {/* <label className={emailNotValid ? "label invalid" : undefined}> */}
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </label>
-          <input
-            type="email"
-            className={emailNotValid ? "invalid" : undefined}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </label>
-          <input
-            type="password"
-            className={passwordNotValid ? "invalid" : undefined}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
+        <Input
+          label="Email"
+          invalid={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
       </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
-      </div>
-    </div>
+      <ActionsContainer>
+        <TextButton type="button">Create a new account</TextButton>
+        <Button onClick={handleLogin}>Sign In</Button>
+      </ActionsContainer>
+    </AuthInputsContainer>
   );
 }
