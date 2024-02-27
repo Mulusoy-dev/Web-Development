a09484e51404cce05b22c53bff9fe8c670c92320
Melih Ulusoy <amelihulusoy@gmail.com>
2024-02-27 10:46:06 +0300

    description is updated

--- Node/node-tut02/middleware/verifyJWT.js
+++ Node/node-tut02/middleware/verifyJWT.js
@@ -7,7 +7,7 @@ const verifyJWT = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   // Başta oluşturulan token 'username' e göre oluşturulduğu için onaylanması da 'username' ile olur.
   // Bu onaylama işlemini 'route' ları korumak için yaptık. Şimdi 'route' da bu onaylama kullanılacak.
-  // İtemciden gelen 'token', backend'de olan 'ACCESS_TOKEN_SECRET' ile çözülür daha sonra karşılaştırılır.
+  // İstemciden gelen 'token', backend'de olan 'ACCESS_TOKEN_SECRET' ile çözülür daha sonra karşılaştırılır.
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.sendStatus(403); // Invalid token
     req.user = decoded.UserInfo.username;
