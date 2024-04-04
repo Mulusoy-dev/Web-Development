f669b583d63b3acf5405c38c584e5cc9ee9b615f
Melih Ulusoy <amelihulusoy@gmail.com>
2024-04-04 15:49:07 +0300

    data fetch todo is added

--- React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
+++ React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // TODO: Fetch available places from backend API
+
   return (
     <Places
       title="Available Places"
