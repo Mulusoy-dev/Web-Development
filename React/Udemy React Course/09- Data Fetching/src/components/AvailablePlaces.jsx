b9d2fc1f722900b57b1d5e8252cf78352bff7a2a
Melih Ulusoy <amelihulusoy@gmail.com>
2024-04-05 15:38:27 +0300

    loading state is added

--- React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
+++ React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
@@ -3,14 +3,17 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [AvailablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   // TODO: Fetch available places from backend API
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true); // Loading...(Fetching...)
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -20,6 +23,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={AvailablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
