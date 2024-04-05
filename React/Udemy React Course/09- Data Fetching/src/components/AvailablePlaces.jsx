567c0914017bf38e2acb3880d072a66a8c474f38
Melih Ulusoy <amelihulusoy@gmail.com>
2024-04-05 09:55:29 +0300

    data retriving from backend via fetch method

--- React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
+++ React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [AvailablePlaces, setAvailablePlaces] = useState([]);
+
   // TODO: Fetch available places from backend API
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={AvailablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
