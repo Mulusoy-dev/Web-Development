caa29bf2b89b0f16345bc713963d38f464cd577f
Melih Ulusoy <amelihulusoy@gmail.com>
2024-04-05 16:06:43 +0300

    error state is added

--- React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
+++ React/Udemy React Course/09- Data Fetching/src/components/AvailablePlaces.jsx
@@ -1,23 +1,40 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [AvailablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   // TODO: Fetch available places from backend API
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true); // Loading...(Fetching...)
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
       setIsFetching(false);
     }
-
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
