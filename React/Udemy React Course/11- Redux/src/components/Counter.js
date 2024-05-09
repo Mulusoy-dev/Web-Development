5f57c2abc5e6046c7b948d5bb3e86c06546352d2
Melih Ulusoy <amelihulusoy@gmail.com>
2024-05-09 15:39:32 +0300

    migrating everyting to redux toolkit

--- React/Udemy React Course/11- Redux/src/components/Counter.js
+++ React/Udemy React Course/11- Redux/src/components/Counter.js
@@ -2,29 +2,31 @@ import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
+import { counterActions } from "../store/index";
+
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decreaseHandler = () => {
-    dispatch({ type: "decrease", amount: 3 });
+    dispatch(counterActions.decrease(3));
   };
 
   const resetHandler = () => {
-    dispatch({ type: "reset" });
+    dispatch(counterActions.reset());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- React/Udemy React Course/11- Redux/src/store/index.js
+++ React/Udemy React Course/11- Redux/src/store/index.js
@@ -1,46 +1,37 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrease") {
-    return {
-      counter: state.counter - action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "reset") {
-    return {
-      counter: 0,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrease(state, action) {
+      state.counter = state.counter - action.payload;
+    },
+    reset(state) {
+      state.counter = 0;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 // Redux store
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
