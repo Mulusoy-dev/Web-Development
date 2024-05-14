95cbbc08903e59a779567cf1e686f8584a43e7f6
Melih Ulusoy <amelihulusoy@gmail.com>
2024-05-14 16:22:06 +0300

    graphqland express are added to node

---
+++ Node/GraphQL/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+    }
+`);
+
+const root = {
+  description: "Protein Bar",
+  price: 3.99,
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Running GraphQL server...");
+});
