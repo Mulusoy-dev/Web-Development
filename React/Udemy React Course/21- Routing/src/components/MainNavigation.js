03877f40ae1e792b06766765e2ba8135ac0fca90
Melih Ulusoy <amelihulusoy@gmail.com>
2024-05-21 16:18:55 +0300

    working with navigation links (Navlink)

--- React/Udemy React Course/21- Routing/src/components/MainNavigation.js
+++ React/Udemy React Course/21- Routing/src/components/MainNavigation.js
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
+// Navlink in Link'ten farkı bir classsname probu alabiliyor. Bu fonksiyon yardımıyla css stilini değiştirebiliyor.
 function MainNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // Varsayılan olarak, bir NavLink, belirtilen to değeri ile URL'in başı eşleştiğinde aktif kabul edilir. Yani, belirtilen to değeri URL'in herhangi bir kısmıyla eşleştiğinde NavLink aktif olur. end özelliği kullanıldığında, NavLink'in aktif kabul edilmesi için URL'in tam olarak eşleşmesi gerekir.
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
