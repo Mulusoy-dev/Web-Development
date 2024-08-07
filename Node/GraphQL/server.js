2a48bb58943b59396bda3f62cc5ce3d042ba2882
Melih Ulusoy <amelihulusoy@gmail.com>
2024-08-07 17:09:19 +0300

    basic e-commerce graphql is added

--- Node/GraphQL/server.js
+++ Node/GraphQL/server.js
@@ -7,16 +7,78 @@ const { ruruHTML } = require("ruru/server");
 
 // To handle GraphQL queries, we need a schema that defines the Query type, and we need an API root with a function called a “resolver” for each API endpoint.
 // E-commerce API example
+// (!=required)
 const schema = buildSchema(`
     type Query {
-    description: String
-    price: Float
+      products: [Product]
+      orders: [Order]
+    }
+
+    type Product {
+      id: ID!
+      description: String!
+      price: Float!
+      reviews: [Review]
+    }
+
+    type Review {
+      rating: Int!
+      comment: String
+    }
+
+    type Order {
+      date: String!
+      subtotal: Float!
+      items: [OrderItem]
+    }
+
+    type OrderItem {
+      product: Product!
+      quantity: Int!
     }
   `);
 
 const root = {
-  description: "T-shirt",
-  price: 30.99,
+  products: [
+    {
+      id: "redshoe",
+      description: "A Red Shoe",
+      price: 20.99,
+      review: [
+        {
+          rating: 6,
+          comment: "A Nice Shoe",
+        },
+      ],
+    },
+    {
+      id: "bluetshirt",
+      description: "A Blue T-shirt",
+      price: 10.99,
+      // review: [
+      //   {
+      //     rating: 3,
+      //     comment: "A Nice T-shirt",
+      //   },
+      // ],
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-06",
+      subtotal: "80.76",
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 5.99,
+          },
+          quantity: 3, // Amount of Shoes
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
